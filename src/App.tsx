8d7254472e7619b1e7717918770edab9a0a534a4
Luis Aleman <lalem014@fiu.edu>
2024-01-06 15:31:58 -0500

    Add fetch functionality and display users

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,39 @@
+import { useState, useEffect } from 'react';
+import User from './components/User';
 import './App.css';
 
-function App() {}
+interface UserDetails {
+  id: number;
+  firstName: string;
+  lastName: string;
+  email: string;
+  birthDate: string;
+  gender: string;
+  address: {
+    state: string;
+  };
+}
+
+function App() {
+  const [users, setUsers] = useState<UserDetails[]>([]);
+
+  useEffect(() => {
+    fetch('https://dummyjson.com/users')
+      .then((response) => response.json())
+      .then((data) => setUsers(data.users))
+      .catch((error) => console.error('Error fetching users:', error));
+  }, []);
+  return (
+    <>
+      <div className="bg-gray-100 p-8">
+        <div>
+          {users.map((user) => (
+            <User key={user.id} user={user} />
+          ))}
+        </div>
+      </div>
+    </>
+  );
+}
 
 export default App;
